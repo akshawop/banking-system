a6b6c8c5fecd78b7bf28419d9e14a3bbbc15e697
akshawop <abhishekshaw1507@gmail.com>
2025-07-01 00:30:54 +0530

    refactor: enhance BankCLI initialization and help menu structure and implemented update bank feature

--- src/me/akshawop/banking/cli/BankCLI.java
+++ src/me/akshawop/banking/cli/BankCLI.java
@@ -7,14 +7,59 @@ import me.akshawop.banking.sys.Bank;
 import me.akshawop.banking.sys.BankDAO;
 
 public final class BankCLI extends BankDAO {
+    static Scanner in = new Scanner(System.in);
+    static Bank bank;
+    static BankCLI dao = new BankCLI();
+
+    BankCLI() {
+        super();
+    }
+
+    BankCLI(Bank bank) {
+        super(bank);
+    }
+
     private static boolean doesBankAlreadyExists() {
-        return BankDAO.getBank() != null;
+        return dao.fetchBank() != null;
+    }
+
+    private static void init() {
+        if (doesBankAlreadyExists()) {
+            bank = dao.fetchBank();
+        } else {
+            System.out.println("\nNo Bank exists, create a new one!");
+            Bank newBank = CreateBankForm.fillUp(in); // form fill up
+            if (newBank == null) {
+                System.out.println("Program stopped successfully");
+                System.exit(0);
+            }
+            dao.addBank(newBank);
+            bank = dao.fetchBank();
+        }
+    }
+
+    private static void updateBank() {
+        Bank newBank = UpdateBankForm.fillUp(in, bank);
+        if (newBank != null) {
+            if (dao.updateBankData(newBank) == 0) {
+                bank = dao.fetchBank();
+                System.out.println("\nBank update Successful!");
+                return;
+            } else
+                System.err.println("\nUpdate unsuccessful!");
+        } else
+            System.out.println("\nUpdate cancelled!");
+    }
+
+    private static void createBranch() {
+        
     }
 
     private static void help() {
         System.out.println("        --HELP MENU--");
         System.out.println("[options -> descriptions]\n");
         System.out.println("exit -> logout");
+        System.out.println("info -> Get Bank Info");
         System.out.println("updatebank -> Update the bank info");
         System.out.println("createbranch -> Create a new Branch");
         System.out.println("closebranch -> Close an existing Branch");
@@ -26,64 +71,51 @@ public final class BankCLI extends BankDAO {
     }
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
 
         try {
-            Bank bank;
-
-            // checking for existing bank
-            if (doesBankAlreadyExists()) {
-                bank = BankDAO.getBank();
-            } else {
-                System.out.println("\nNo Bank exists, create a new one!");
-                Bank newBank = CreateBankForm.fillUp(in); // form fill up
-                if (newBank == null) {
-                    System.out.println("Program stopped successfully");
-                    System.exit(0);
-                }
-                BankDAO.addBank(newBank);
-                bank = BankDAO.getBank();
-            }
-
             // start
-            BankDAO.bank = bank;
+            init();
             System.out.println("\n    " + bank.getBankName().toUpperCase() + "\n");
             help();
 
             String input;
             do {
                 System.out.print("bank> ");
-                input = in.next();
-                in.nextLine();
+                input = in.nextLine().toLowerCase();
+                // in.nextLine();
 
                 switch (input) {
                     case "updatebank":
-                        // update
-                        BankDAO.updateBankData(bank);
+                        // update bank
+                        updateBank();
                         break;
                     case "createbranch":
                         // add branch
-                        BankDAO.addBranch(null);
+                        createBranch();
                         break;
                     case "closebranch":
                         // remove branch
-                        BankDAO.removeBranch(null);
+                        dao.removeBranch(null);
                         break;
                     case "branchlogin":
                         // get branch
-                        BankDAO.getBranch(null);
+                        dao.getBranch(null);
                         break;
                     case "listbranches":
                         // list branches
-                        BankDAO.listBranches();
+                        dao.listBranches();
                         break;
                     case "listcustomers":
                         // list customers
-                        BankDAO.listCustomers(0, 0);
+                        dao.listCustomers(0, 0);
                         break;
                     case "listaccounts":
                         // list accounts
-                        BankDAO.listAccounts(0, 0);
+                        dao.listAccounts(0, 0);
+                        break;
+                    case "info":
+                        // print bank info
+                        dao.showBankInfo();
                         break;
                     case "help":
                         // show help menu
@@ -96,6 +128,7 @@ public final class BankCLI extends BankDAO {
                         System.out.println("invalid input!");
                 }
             } while (!input.equals("exit"));
+            System.out.println("Program stopped successfully");
         } catch (Exception e) {
             System.err.println(e);
             System.err.println("\nProgram stopped Abnormally!");
