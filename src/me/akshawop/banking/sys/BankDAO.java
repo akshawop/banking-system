9596080ae37850955a50442796343740cf62d968
akshawop <abhishekshaw1507@gmail.com>
2025-07-01 00:28:30 +0530

    refactor: update BankDAO to use instance methods and improve database handling and implemented some methods

--- src/me/akshawop/banking/sys/BankDAO.java
+++ src/me/akshawop/banking/sys/BankDAO.java
@@ -6,21 +6,41 @@ import me.akshawop.banking.cli.BankCLI;
 import me.akshawop.banking.sql.SQLQueries;
 
 public sealed class BankDAO permits BankCLI {
-    protected static Bank bank;
+    private Bank bank;
 
-    protected static Bank getBank() {
+    protected BankDAO() {
+        Bank dbBank = fetchBank();
+        if (dbBank != null)
+            this.bank = dbBank;
+    }
+
+    protected BankDAO(Bank bank) {
+        this.bank = bank;
+    }
+
+    protected void setBank(Bank bank) {
+        this.bank = bank;
+    }
+
+    protected void showBankInfo() {
+        System.out.println("\nBank: " + bank.getBankName().toUpperCase());
+        System.out.println("Bank Code: " + bank.getBankCode() + "\n");
+    }
+
+    protected Bank fetchBank() {
         // getBank
-        Connection con = DB.connect();
         try {
+            Connection con = DB.connect();
             Statement st = con.createStatement();
-            ResultSet rs = st.executeQuery(SQLQueries.getBank());
-            con.close();
-            if (rs.isClosed()) {
+            ResultSet rs = st.executeQuery(SQLQueries.fetchBankFromDB());
+
+            if (!rs.next()) {
+                con.close();
                 return null;
             } else {
-                rs.next();
                 String bankCode = rs.getString("bank_code");
                 String bankName = rs.getString("bank_name");
+                con.close();
                 return new Bank(bankCode, bankName);
             }
         } catch (SQLTimeoutException e) {
@@ -36,36 +56,77 @@ public sealed class BankDAO permits BankCLI {
         return null;
     }
 
-    protected static void addBank(Bank bank) {
+    protected int addBank(Bank bank) {
         // addBankToDB
+        try {
+            Connection con = DB.connect();
+            Statement st = con.createStatement();
+            st.executeUpdate(SQLQueries.addBankToDB(bank));
+            con.close();
+            this.bank = fetchBank();
+            return 0;
+        } catch (SQLTimeoutException e) {
+            System.err.println("Error: Database timeout!");
+            System.err.println("More info:\n" + e);
+            return 1;
+        } catch (SQLException e) {
+            System.err.println("Error: Database Access Error!");
+            System.err.println("More info:\n" + e);
+            return 1;
+        } catch (Exception e) {
+            System.err.println("Error: something went wrong!");
+            System.err.println("More info:\n" + e);
+            return 1;
+        }
     }
 
-    protected static void updateBankData(Bank bank) {
+    protected int updateBankData(Bank bank) {
         // updateBankData
+        try {
+            Connection con = DB.connect();
+            Statement st = con.createStatement();
+            st.executeUpdate(SQLQueries.updateBankInDB(bank));
+            con.close();
+            this.bank = fetchBank();
+            return 0;
+        } catch (SQLTimeoutException e) {
+            System.err.println("Error: Database timeout!");
+            System.err.println("More info:\n" + e);
+            return 1;
+        } catch (SQLException e) {
+            System.err.println("Error: Database Access Error!");
+            System.err.println("More info:\n" + e);
+            return 1;
+        } catch (Exception e) {
+            System.err.println("Error: something went wrong!");
+            System.err.println("More info:\n" + e);
+            return 1;
+        }
     }
 
-    protected static void addBranch(Branch branch) {
+    protected void addBranch(Branch branch) {
         // addBranchToDB
     }
 
-    protected static void removeBranch(String branchCode) {
+    protected void removeBranch(String branchCode) {
         // removeBranch
     }
 
-    protected static Branch getBranch(String branchCode) {
+    protected Branch getBranch(String branchCode) {
         // getBranch
         return null;
     }
 
-    protected static void listBranches() {
+    protected void listBranches() {
         // listBranches
     }
 
-    protected static void listCustomers(int from, int limit) {
+    protected void listCustomers(int from, int limit) {
 
     }
 
-    protected static void listAccounts(int from, int limit) {
+    protected void listAccounts(int from, int limit) {
 
     }
+
 }
