15828e60143b156ffa4e68fca80cf7abe43ce675
akshawop <abhishekshaw1507@gmail.com>
2025-06-30 21:52:46 +0530

    refactor: add copy constructor and setters for bank properties

--- src/me/akshawop/banking/sys/Bank.java
+++ src/me/akshawop/banking/sys/Bank.java
@@ -9,11 +9,24 @@ public class Bank {
         this.bankName = bankName;
     }
 
+    public Bank(Bank otherBank) {
+        this.bankCode = otherBank.getBankCode();
+        this.bankName = otherBank.getBankName();
+    }
+
     public String getBankCode() {
         return bankCode;
     }
 
     public String getBankName() {
         return bankName;
+    }
+
+    public void setBankCode(String bankCode) {
+        this.bankCode = bankCode;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
     }
 }
