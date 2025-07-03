0c761b5c4962de29dda8637e62b0f925c10980d7
akshawop <abhishekshaw1507@gmail.com>
2025-07-03 22:49:49 +0530

    feat: implement UpdateAddress class for user input and validation

---
+++ src/me/akshawop/banking/inputmodules/UpdateAddress.java
@@ -0,0 +1,76 @@
+package me.akshawop.banking.inputmodules;
+
+import java.util.Scanner;
+
+import me.akshawop.banking.customtype.Address;
+
+public class UpdateAddress {
+    private static Address address;
+
+    private static boolean isValidAddress(String str) {
+        return (str.length() == 0 || str.length() <= 50);
+    }
+
+    private static boolean isValidPinCode(String pinCode) {
+        return (pinCode.length() == 0 || pinCode.length() == 6);
+    }
+
+    public static Address input(Address recvAddress, Scanner in) {
+        address = new Address(recvAddress);
+        // street
+        do {
+            System.out.print("Street: ");
+            address.street = in.nextLine().toLowerCase().trim();
+            if (!isValidAddress(address.street)) {
+                System.out.println("Wrong input: Cannot be greater than 50 characters");
+                continue;
+            }
+            break;
+        } while (true);
+
+        // city
+        do {
+            System.out.print("City: ");
+            address.city = in.nextLine().toLowerCase().trim();
+            if (!isValidAddress(address.city)) {
+                System.out.println("Wrong input: Cannot be greater than 50 characters");
+                continue;
+            }
+            break;
+        } while (true);
+
+        // district
+        do {
+            System.out.print("District: ");
+            address.district = in.nextLine().toLowerCase().trim();
+            if (!isValidAddress(address.district)) {
+                System.out.println("Wrong input: Cannot be greater than 50 characters");
+                continue;
+            }
+            break;
+        } while (true);
+
+        // state
+        do {
+            System.out.print("State: ");
+            address.state = in.nextLine().toLowerCase().trim();
+            if (!isValidAddress(address.state)) {
+                System.out.println("Wrong input: Cannot be greater than 50 characters");
+                continue;
+            }
+            break;
+        } while (true);
+
+        // pinCode
+        do {
+            System.out.print("Area PIN Code: ");
+            address.pinCode = in.nextLine().toLowerCase().trim();
+            if (!isValidPinCode(address.pinCode)) {
+                System.out.println("Wrong input: Has to be 6 characters long");
+                continue;
+            }
+            break;
+        } while (true);
+        return address;
+    }
+}
