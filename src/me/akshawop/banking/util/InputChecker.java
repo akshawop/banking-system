254614bd6fcdd8568f45a7258a0fe26e3880030e
akshawop <abhishekshaw1507@gmail.com>
2025-07-03 23:04:48 +0530

    refactor: simplify character type checks in InputChecker methods

--- src/me/akshawop/banking/util/InputChecker.java
+++ src/me/akshawop/banking/util/InputChecker.java
@@ -3,30 +3,37 @@ package me.akshawop.banking.util;
 public class InputChecker {
 
     public static boolean checkBankName(String bankName, char type) {
-        if (type == 'o' || type == 'O')
+        if (Character.toLowerCase(type) == 'o')
             return bankName.length() <= 100;
         else
             return (bankName.length() > 0 && bankName.length() <= 100);
     }
 
     public static boolean checkBankCode(String bankCode, char type) {
-        if (type == 'o' || type == 'O')
+        if (Character.toLowerCase(type) == 'o')
             return (bankCode.length() == 0 || bankCode.length() == 4);
         else
             return bankCode.length() == 4;
     }
 
     public static boolean checkBranchName(String branchName, char type) {
-        if (type == 'o' || type == 'O')
+        if (Character.toLowerCase(type) == 'o')
             return branchName.length() <= 50;
         else
             return (branchName.length() > 0 && branchName.length() <= 50);
     }
 
     public static boolean checkBranchCode(String branchCode, char type) {
-        if (type == 'o' || type == 'O')
-            return branchCode.length() == 0 || branchCode.length() == 6;
+        if (Character.toLowerCase(type) == 'o')
+            return (branchCode.length() == 0 || branchCode.length() == 6);
         else
             return branchCode.length() == 6;
+    }
+
+    public static boolean checkName(String name, char type) {
+        if (Character.toLowerCase(type) == 'o')
+            return (name.length() == 0 || name.length() <= 25);
+        else
+            return (name.length() > 0 && name.length() <= 25);
     }
 }
