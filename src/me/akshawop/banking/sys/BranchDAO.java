fb54fff88bfcf92f471d7ff198e484a362a33c2f
akshawop <abhishekshaw1507@gmail.com>
2025-07-05 00:11:09 +0530

    refactor: update BranchDAO constructor to initialize branch with data from ResultSet, create getter for branch and create method to check if any branch exist in the DB

--- src/me/akshawop/banking/sys/BranchDAO.java
+++ src/me/akshawop/banking/sys/BranchDAO.java
@@ -1,10 +1,66 @@
 package me.akshawop.banking.sys;
 
+import java.sql.*;
+
+import me.akshawop.banking.customtype.Address;
+import me.akshawop.banking.sql.SQLQueries;
+
 public class BranchDAO {
     private Branch branch;
 
-    protected BranchDAO(Branch branch) {
-        this.branch = branch;
+    protected BranchDAO(String branchCode) {
+        try {
+            Connection con = DB.connect();
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(SQLQueries.getBranchFromDB(branchCode));
+
+            if (rs.next()) {
+                int branchId = rs.getInt("branch_id");
+                String branchName = rs.getString("branch_name");
+                Address address = new Address(rs);
+                Date openingDate = rs.getDate("opening_date");
+                branch = new Branch(branchId, branchCode, branchName, address, openingDate);
+            }
+            con.close();
+        } catch (SQLTimeoutException e) {
+            System.err.println("Error: Database timeout!");
+            System.err.println("More info:\n" + e);
+        } catch (SQLException e) {
+            System.err.println("Error: Database Access Error!");
+            System.err.println("More info:\n" + e);
+        } catch (Exception e) {
+            System.err.println("Error: something went wrong!");
+            System.err.println("More info:\n" + e);
+        }
+    }
+
+    protected Branch getCurrentBranch() {
+        return branch;
+    }
+
+    public static boolean doesAnyBranchExists() {
+        // checks for any pre-existing branch
+        try {
+            Connection con = DB.connect();
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(SQLQueries.firstBranchInDB());
+
+            if (rs.next()) {
+                con.close();
+                return true;
+            }
+            con.close();
+        } catch (SQLTimeoutException e) {
+            System.err.println("Error: Database timeout!");
+            System.err.println("More info:\n" + e);
+        } catch (SQLException e) {
+            System.err.println("Error: Database Access Error!");
+            System.err.println("More info:\n" + e);
+        } catch (Exception e) {
+            System.err.println("Error: something went wrong!");
+            System.err.println("More info:\n" + e);
+        }
+        return false;
     }
 
     protected void addCustomer(Customer customer) {
