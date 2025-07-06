e35ffc682f7c534bbcba25a6a44e53a3d255bb50
akshawop <abhishekshaw1507@gmail.com>
2025-07-06 19:20:35 +0530

    fix: fixed the empty field bug when input is skipped for a field while updating the address

--- src/me/akshawop/banking/inputmodules/UpdateAddress.java
+++ src/me/akshawop/banking/inputmodules/UpdateAddress.java
@@ -6,6 +6,11 @@ import me.akshawop.banking.customtype.Address;
 
 public class UpdateAddress {
     private static Address address;
+    private static String street;
+    private static String city;
+    private static String district;
+    private static String state;
+    private static String pinCode;
 
     private static boolean isValidAddress(String str) {
         return (str.length() == 0 || str.length() <= 50);
@@ -15,62 +20,68 @@ public class UpdateAddress {
         return (pinCode.length() == 0 || pinCode.length() == 6);
     }
 
-    public static Address input(Address recvAddress, Scanner in) {
+    public static Address input(Scanner in, Address recvAddress) {
         address = new Address(recvAddress);
         // street
         do {
             System.out.print("Street: ");
-            address.street = in.nextLine().toLowerCase().trim();
-            if (!isValidAddress(address.street)) {
+            street = in.nextLine().toLowerCase().trim();
+            if (!isValidAddress(street)) {
                 System.out.println("Wrong input: Cannot be greater than 50 characters");
                 continue;
             }
             break;
         } while (true);
+        address.street = street.length() == 0 ? address.street : street;
 
         // city
         do {
             System.out.print("City: ");
-            address.city = in.nextLine().toLowerCase().trim();
-            if (!isValidAddress(address.city)) {
+            city = in.nextLine().toLowerCase().trim();
+            if (!isValidAddress(city)) {
                 System.out.println("Wrong input: Cannot be greater than 50 characters");
                 continue;
             }
             break;
         } while (true);
+        address.city = city.length() == 0 ? address.city : city;
 
         // district
         do {
             System.out.print("District: ");
-            address.district = in.nextLine().toLowerCase().trim();
-            if (!isValidAddress(address.district)) {
+            district = in.nextLine().toLowerCase().trim();
+            if (!isValidAddress(district)) {
                 System.out.println("Wrong input: Cannot be greater than 50 characters");
                 continue;
             }
             break;
         } while (true);
+        address.district = district.length() == 0 ? address.district : district;
 
         // state
         do {
             System.out.print("State: ");
-            address.state = in.nextLine().toLowerCase().trim();
-            if (!isValidAddress(address.state)) {
+            state = in.nextLine().toLowerCase().trim();
+            if (!isValidAddress(state)) {
                 System.out.println("Wrong input: Cannot be greater than 50 characters");
                 continue;
             }
             break;
         } while (true);
+        address.state = state.length() == 0 ? address.state : state;
 
         // pinCode
         do {
             System.out.print("Area PIN Code: ");
-            address.pinCode = in.nextLine().toLowerCase().trim();
-            if (!isValidPinCode(address.pinCode)) {
+            pinCode = in.nextLine().toLowerCase().trim();
+            if (!isValidPinCode(pinCode)) {
                 System.out.println("Wrong input: Has to be 6 characters long");
                 continue;
             }
             break;
         } while (true);
+        address.pinCode = pinCode.length() == 0 ? address.pinCode : pinCode;
+
         return address;
     }
 }
