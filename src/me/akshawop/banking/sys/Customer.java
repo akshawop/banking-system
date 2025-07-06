45790f90cb263fe41451943cb0087febe7d5971f
akshawop <abhishekshaw1507@gmail.com>
2025-07-06 16:34:34 +0530

    feat: made Customer constructor to accept ResultSet as parameter with exception handling for easy initialization

--- src/me/akshawop/banking/sys/Customer.java
+++ src/me/akshawop/banking/sys/Customer.java
@@ -1,6 +1,8 @@
 package me.akshawop.banking.sys;
 
 import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import me.akshawop.banking.customtype.Address;
 
@@ -23,16 +25,24 @@ public class Customer {
         this.email = email;
     }
 
-    public Customer(int customerId, String name, String aadhaar, String pan, Address address, String phone,
-            String email, Date registrationDate) {
-        this.customerId = customerId;
-        this.name = name;
-        this.aadhaar = aadhaar;
-        this.pan = pan;
-        this.address = address;
-        this.phone = phone;
-        this.email = email;
-        this.registrationDate = registrationDate;
+    public Customer(ResultSet rs) {
+        try {
+            String midName = rs.getString("mid_name");
+            this.name = rs.getString("first_name") + " " + midName + (midName.length() == 0 ? "" : " ")
+                    + rs.getString("last_name");
+            this.aadhaar = rs.getString("aadhaar");
+            this.pan = (rs.getString("pan") == null ? "" : rs.getString("pan"));
+            this.address = new Address(rs);
+            this.phone = rs.getString("phone");
+            this.email = (rs.getString("email") == null ? "" : rs.getString("email"));
+            this.registrationDate = rs.getDate("registration_date");
+        } catch (SQLException e) {
+            System.err.println("Cannot access the Database while creating new Address object!");
+            System.err.println("More info:\n" + e);
+        } catch (Exception e) {
+            System.err.println("something went wrong while creating new Address object!");
+            System.err.println("More info:\n" + e);
+        }
     }
 
     public int getCustomerId() {
